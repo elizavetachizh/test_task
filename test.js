e9c083320334583d4fffe2509fa165c7d51593a8
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2021-09-02 14:43:21 +0300

    new_commit

--- test.js
+++ test.js
@@ -1,20 +1,22 @@
-let arr =[]
-const a=20
-for ( let i=1; i<=a; i++) {
-arr.push(i)
+const arrs = "lrft";
+const arrx = arrs.split("");
+const arrt = arrx.reverse().join("");
+console.log(arrt);
+
+let arr = [];
+const a = 20;
+for (let i = 1; i <= a; i++) {
+  arr.push(i);
 }
-for(let i=1; i<a; i++) {
-if (i%3===0) {
-arr[i-1]='three'
+for (let i = 1; i < a; i++) {
+  if (i % 3 === 0) {
+    arr[i - 1] = "three";
+  } else if (i % 5 === 0) {
+    arr[i - 1] = "five";
+  } else if (i % 3 === 0 && i % 5 === 0) {
+    arr[i - 1] = "threefive";
+  } else {
+    console.log(i);
+  }
 }
-else if (i%5===0) {
-arr[i-1]='five'
-}
-else if (i % 3===0 && i % 5===0) {
-arr[i-1]='threefive'
-}
-else {
-console.log(i)
-}
-}
-console.log(arr)
+console.log(arr.join());
