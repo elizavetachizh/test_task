ac516848a08a71e701e757a05113e6391e05bd48
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2021-09-02 14:19:47 +0300

    new_task

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-dfghjk;lkjhg
+let arr =[]
+const a=20
+for ( let i=1; i<=a; i++) {
+arr.push(i)
+}
+for(let i=1; i<a; i++) {
+if (i%3===0) {
+arr[i-1]='three'
+}
+else if (i%5===0) {
+arr[i-1]='five'
+}
+else if (i % 3===0 && i % 5===0) {
+arr[i-1]='threefive'
+}
+else {
+console.log(i)
+}
+}
+console.log(arr)
